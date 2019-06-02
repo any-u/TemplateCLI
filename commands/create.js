9fd377fc6550fd309ac358b00bed590187660662
sillyy <965768042@qq.com>
2019-06-02 18:19:44 +0800

    add 格式检测, 路径创建

--- commands/create.js
+++ commands/create.js
@@ -4,8 +4,10 @@ const chalk = require('chalk')
 const ora = require('ora')
 const fs = require('fs')
 const path = require('path')
+const mkdirp = require('mkdirp');
 
-const log = console.log
+const log = console.log,
+  getDirName = require('path').dirname;
 
 const option = program.parse(process.argv).args,
   dir = option[0],
@@ -35,21 +37,33 @@ const question = [
   }
 ]
 
-const writeFile = (path, data) => {
+const writeFile = (path, contents) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(path, data, 'utf8', function (err) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve()
-      }
-    })
+    mkdirp(getDirName(path), function (err) {
+      if (err) return reject(err);
+      fs.writeFile(path, contents, (err) => {
+        err ? reject(err) : resolve()
+      });
+    });
   })
 }
 
 module.exports = prompt(question).then(({ frame }) => {
-  const spinner = ora('生成模板中...\n'),
-    format = frame === 'React' ? '.js' : '.vue'
+  const format = frame === 'React' ? '.js' : '.vue',
+    checkFormat = ora('检查格式中...\n')
+
+  // 识别文件名和format格式是否一致
+  checkFormat.start()
+  files.forEach(item => {
+    if (!item.includes(format) ) {
+      checkFormat.fail()
+      log(chalk.red("请选择正确格式"))
+      process.exit()
+    }
+  });
+  checkFormat.stop()
+
+  const spinner = ora('生成模板中...\n')
   spinner.start()
   var a = path.resolve(`./templates/${frame}/index${format}`)
   fs.readFile(a, 'utf8', async (err, data) => {
@@ -67,16 +81,16 @@ module.exports = prompt(question).then(({ frame }) => {
           (name.charAt(0).toUpperCase() + name.slice(1)).replace(format, '')
         )
         try {
-          return await writeFile(`./${dir}/${name}`, template)
+          await writeFile(`./${dir}/${name}`, template)
+          return
         } catch (e) {
           spinner.fail()
           log(chalk.red(e))
           process.exit()
-          return
         }
       })
     )
-    log(chalk.green(`文件生成成功`))
     spinner.stop()
+    log(chalk.green(`模板生成成功`))
   })
 })
